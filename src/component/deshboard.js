bfc7341d775adf504075d295b9efbcdfb852f99d
pansuriyaakshay <pansuriyaakshay28@gmail.com>
2023-10-24 13:47:57 +0530

    -

---
+++ src/component/deshboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const deshboard = () => {
+  return (
+    <div>deshboard</div>
+  )
+}
+
+export default deshboard
---
+++ src/component/footter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const footter = () => {
+  return (
+    <div>footter</div>
+  )
+}
+
+export default footter
